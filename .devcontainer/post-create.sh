71db053d5b92069821820832119e1db759c568df
wilson-dynatrace <53265413+wilson-dynatrace@users.noreply.github.com>
2024-10-13 20:35:42 +0800

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,6 +7,6 @@ kind create cluster --config .devcontainer/kind-cluster.yml --wait 300s
 
 # Setup Hipstershop
 kubectl create ns hipster-shop
-kubectl -n hipster-shop apply -f kubernetes-manifests.yaml
+kubectl -n hipster-shop apply -f k8s-manifest.yaml
 
 echo "[post-create] complete" >> ~/status
